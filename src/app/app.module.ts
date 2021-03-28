8e25c652e6ff4f8db87d0b15f5e2fb024965a1ba
Jordan <jordanandrade@outlook.com>
2021-03-28 23:30:05 +0200

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+
+import { FormulaireComponent } from './formulaire/formulaire.component';
+import { HeaderComponent } from './header/header.component';
+import { IdentificationGuardGuard } from './identification-guard.guard';
+import { NgxsModule } from '@ngxs/store';
+import { HomeComponent } from './home/home.component';
+import {PanierState} from './shared/states/panier-state';
+
+
+const appLiens : Routes = [
+  {path:'formulaire',component:FormulaireComponent},
+  {path:'home',component:HomeComponent},
+  {path:'produit', canActivate: [IdentificationGuardGuard], loadChildren:() => import('./produit/produit.module').then(m => m.ProduitModule)}
+]
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,FormsModule,HttpClientModule,RouterModule.forRoot (appLiens), 
+    NgxsModule.forRoot([PanierState])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
